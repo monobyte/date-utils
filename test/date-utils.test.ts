f3a170fb069b6f0edefe5817dc7752fb86ce8dba
Dan Carter <danielrosscarter@gmail.com>
2021-12-08 22:01:02 +0000

    Added docs to visualise ranges

--- test/date-utils.test.ts
+++ test/date-utils.test.ts
@@ -31,34 +31,68 @@ describe('DateUtils', () => {
 
         describe('intersecting dates', () => {
             it('is true when date range A overlaps date range B', () => {
+                /**
+                 *      |------------|
+                 *             |----------|
+                 */
                 expect(DateUtils.doDatesRangesIntersect(jan01ToJan10, jan05ToJan30)).toBeTruthy();
+                /**
+                 *      |------------|
+                 *           |----|
+                 */
                 expect(DateUtils.doDatesRangesIntersect(jan01ToJan10, jan05ToJan09)).toBeTruthy();
-                expect(DateUtils.doDatesRangesIntersect(jan05ToJan09, jan01ToJan10)).toBeTruthy();
+                /**
+                 *            |-----------|
+                 *      |------------|
+                 */
+                expect(DateUtils.doDatesRangesIntersect(jan05ToJan30, jan01ToJan10)).toBeTruthy();
+                /**
+                 *            |-----|
+                 *      |------------|
+                 */
                 expect(DateUtils.doDatesRangesIntersect(jan05ToJan09, jan01ToJan10)).toBeTruthy();
             });
 
             it(`is false when range A doesn't overlap range B`, () => {
-                // Range A starts first
+                /**
+                 *      |-----|
+                 *                 |-----|
+                 */
                 expect(DateUtils.doDatesRangesIntersect(jan01ToJan10, feb01toFeb10)).toBeFalsy();
-                // Range B starts first
+                /**
+                 *                 |-----|
+                 *      |-----|
+                 */
                 expect(DateUtils.doDatesRangesIntersect(feb01toFeb10, jan01ToJan10)).toBeFalsy();
             });
         });
 
         describe('missing start dates', () => {
             it('is true when range B starts before end of range A', () => {
+                /**
+                 *   --------------|
+                 *        |-----|
+                 */
                 expect(
                     DateUtils.doDatesRangesIntersect(missingStartDateToJan30, jan01ToJan10)
                 ).toBeTruthy();
             });
 
             it('is true when range B starts before range A and they overlap', () => {
+                /**
+                 *        |-----|
+                 *   --------------|
+                 */
                 expect(
                     DateUtils.doDatesRangesIntersect(jan01ToJan10, missingStartDateToJan30)
                 ).toBeTruthy();
             });
 
             it(`is false when range A doesn't overlap range B`, () => {
+                /**
+                 *   --------------|
+                 *                      |-----|
+                 */
                 expect(
                     DateUtils.doDatesRangesIntersect(missingStartDateToJan30, feb01toFeb10)
                 ).toBeFalsy();
@@ -67,18 +101,30 @@ describe('DateUtils', () => {
 
         describe('missing end dates', () => {
             it('is true when range B starts within range A', () => {
+                /**
+                 *   |---------------------------
+                 *                 |-----|
+                 */
                 expect(
                     DateUtils.doDatesRangesIntersect(fromJan20MissingEndDate, feb01toFeb10)
                 ).toBeTruthy();
             });
 
             it('is true when range A ends after range B starts', () => {
+                /**
+                 *   |-----|
+                 *      |---------------------------
+                 */
                 expect(
                     DateUtils.doDatesRangesIntersect(jan05ToJan30, fromJan20MissingEndDate)
                 ).toBeTruthy();
             });
 
             it(`is false when range A doesn't overlap range B`, () => {
+                /**
+                 *  |-----|
+                 *          |------------------------
+                 */
                 expect(
                     DateUtils.doDatesRangesIntersect(jan01ToJan10, fromJan20MissingEndDate)
                 ).toBeFalsy();
